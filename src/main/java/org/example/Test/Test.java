ad13043d3afcf642396f523526872d9f99735044
Anton <NotRealEmail.com>
2023-03-09 11:33:56 +0300

    test

--- src/main/java/org/example/Test/Test.java
+++ src/main/java/org/example/Test/Test.java
@@ -3,6 +3,6 @@ package org.example.Test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("Hello");
+      
     }
 }
